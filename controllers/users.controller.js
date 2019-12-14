88aa56ef2a2201ce21709d2f636d70ea80012b0a
Learn2Code <infra@learn2code.pl>
2019-12-14 14:10:16 +0100

    status changed

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -3,6 +3,6 @@ const User = require('../models/user.model');
 module.exports.createUser = function(req, res) {
     let user = new User({name: req.body.name, password: req.body.password});
     user.save((err) => {
-        res.sendStatus(200).json({msg: 'uzytkownik dodany'})
+        res.status(200).json({msg: 'uzytkownik dodany'})
     });
 }
